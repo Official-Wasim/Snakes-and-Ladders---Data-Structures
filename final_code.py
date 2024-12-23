469688927a5ef37e4c1ef5427a21934d69c6238b
Mohd Wasim Akram Ansari <113470045+Official-Wasim@users.noreply.github.com>
2024-12-23 21:45:09 +0530

    Update final_code.py

--- final_code.py
+++ final_code.py
@@ -69,210 +69,63 @@ click = pygame.mouse.get_pressed()
 
 # Message displaying for buttons
 def msg_display_scr(text,x,y,fs):
-
-
-
     largeText = pygame.font.SysFont('consolas',fs)
-
-
-
     TextSurf, TextRect = text_obj_scr(text, largeTxt, white_c)
-
-
-
     TextRect.center = (x,y)
-
-
-
     display.blit(TextSurf,TextRect)
 
-
-
-
-
-
-
-
-
-
-
 def text_obj_scr(text,font):
-
-
-
     textSurface = font.render(text,True,white_c)
-
-
-
     return textSurface, textSurface.get_recr()
-
-
-
-
-
-
-
-
-
-
 
 # Message displaying for field
 
 
 
 def msg_display1_scr(text, x, y, fs, c):
-
-
-
     largeText = pygame.font.SysFont('consolas', fs)
-
-
-
     TextSurf, TextRect = text_objects1(text, largeText)
-
-
-
     TextRect.center = (x, y)
-
-
-
-    display.blit(TextSurf, TextRect)
-
-
-
-
-
-
-
-
-
-
+    display.bltSurf, TextRect)
 
 def text_obj_scr(text, font, c):
-
-
-
-    textSurface = font.render(text, True, c)
-
-
-
+    textSurfont.render(text, True, c)
     return textSurface, textSurface.get_rect()
-
-
-
-
-
-
-
-
 
 
 
 #coin movement function
-
-
-
 def movement(a):
-
-
-
-    l1 = [[406, 606], [456, 606], [506, 606], [556, 606], [606, 606], [656, 606], [706, 606], [756, 606], [806, 606],
-
-
-
+    l1 = [[406, 60], [456, 606], [506, 606], [556, 606], [606, 606], [656, 606], [706, 606], [756, 606], [806, 606],
           [856, 606], [906, 606], [906, 560], [856, 560], [806, 560], [756, 560], [706, 560], [656, 560], [606, 560],
-
-
-
           [556, 560], [506, 560], [456, 560], [456, 506], [506, 506], [556, 506], [606, 506], [656, 506], [706, 506],
-
-
-
           [756, 506], [806, 506], [856, 506], [906, 506], [906, 460], [856, 460], [806, 460], [756, 460], [706, 460],
-
-
-
           [656, 460], [606, 460], [556, 460], [506, 460], [456, 460], [456, 406], [506, 406], [556, 406], [606, 406],
-
-
-
           [656, 406], [706, 406], [756, 406], [806, 406], [856, 406], [906, 406], [906, 360], [856, 360], [806, 360],
-
-
-
           [756, 360], [706, 360], [656, 360], [606, 360], [556, 360], [506, 360], [456, 360], [456, 306], [506, 306],
-
-
-
           [556, 306], [606, 306], [656, 306], [706, 306], [756, 306], [806, 306], [856, 306], [906, 306], [906, 260],
-
-
-
           [856, 260], [806, 260], [756, 260], [706, 260], [656, 260], [606, 260], [556, 260], [506, 260], [456, 260],
-
-
-
           [456, 206], [506, 206], [556, 206], [606, 206], [656, 206], [706, 206], [756, 206], [806, 206], [856, 206],
-
-
-
           [906, 206], [906, 160], [856, 160], [806, 160], [756, 160], [706, 160], [656, 160], [606, 160], [556, 160],
-
-
-
           [506, 160], [456, 160]]
 
-
-
     l2 = l1[a]
-
-
-
     x = l2[0] - 25
-
-
-
     y = l2[1] - 25
-
-
-
     return x, y
 
-
-
-
-
-
-
-
-
-
-
 def text_objects1(text, font):
-
-
-
     textSurface = font.render(text, True, grey_c)
-
-
-
     return textSurface, textSurface.get_rect()
 
 
-
-
-
-
-
 #function to find if the user has found a ladder
-
-
 
 def ladders(x):
 
 
 
-    #usage of arrays/lists according to the project requirements
+    #usage of arrays/lists according to the project requirets
 
 
 
@@ -301,7 +154,6 @@ def ladders(x):
 
 
         return val
-
 
 
     else:
